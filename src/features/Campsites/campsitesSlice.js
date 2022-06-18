8220ee36b384f4709f382cf646070e92868183d8
lalaloopsi213 <fg1041@gmail.com>
2022-06-18 11:02:32 -0500

    Router Setup

--- src/features/Campsites/campsitesSlice.js
+++ src/features/Campsites/campsitesSlice.js
@@ -1,11 +1,12 @@
-import { CAMPSITES} from '../../app/shared/CAMPSITES';
+import { CAMPSITES } from '../../app/shared/CAMPSITES';
 
-export const selectAllCampsites= () => {
+export const selectAllCampsites = () => {
     return CAMPSITES;
 };
 
-export const selectRandomCampsite = () => {
-    return CAMPSITES[Math.floor(CAMPSITES.length * Math.random())];
+export const selectCampsiteById = (id) => {
+    return CAMPSITES.find((campsite) => campsite.id === id);
 };
+
 
 
