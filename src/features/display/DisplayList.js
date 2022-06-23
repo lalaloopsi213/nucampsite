a8613c2a8f98c8d73e67570be7e8d79fcfc4e8e7
lalaloopsi213 <fg1041@gmail.com>
2022-06-23 16:32:51 -0500

    commit on main

--- src/features/display/DisplayList.js
+++ src/features/display/DisplayList.js
@@ -1,17 +1,22 @@
 import { Col, Row} from 'reactstrap';
-import DisplayCard from './DisplayCard';
+import AnimatedDisplayCard from './AnimatedDisplayCard';
 import { selectFeaturedCampsite } from '../Campsites/campsitesSlice';
 import { selectFeaturedPromotion } from '../promotions/promotionsSlice';
+import {selectFeaturedPartner} from '../partners/partnersSlice';
 
 const DisplayList = () => {
-    const items = [selectFeaturedCampsite(), selectFeaturedPromotion()];
+    const items = [
+        selectFeaturedCampsite(),
+        selectFeaturedPromotion(),
+        selectFeaturedPartner()
+    ];
 
     return(
         <Row>
             {items.map((item, idx) => {
                 return (
                     <Col md className='m-1' key={idx}>
-                        <DisplayCard item={item} />
+                        <AnimatedDisplayCard item={item} />
                     </Col> 
                 );
             })}
